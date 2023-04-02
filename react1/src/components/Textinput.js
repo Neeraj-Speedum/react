8457fbcbc9bfbb4a149a12930873ba0d0df17ae6
Neeraj <nsharma1@speedum.co.in>
2023-04-02 14:12:09 +0530

    done

---
+++ react1/src/components/Textinput.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+export default function MyInput() {
+  const [text, setText] = useState("hello");
+
+  function handleChange(e) {
+    setText(e.target.value);
+    
+  }
+
+  return (
+    <>
+    <div className="container">
+      <input value={text} onChange={handleChange} />
+      <p>You typed: {text}</p>
+      <button onClick={() => setText("done")}>Reset</button>
+      </div>
+    </>
+  );
+}
